7976825c7a855fea834ae37cec6be35d8881d493
kimbyeolhee <qufgml0216@naver.com>
2023-05-09 01:47:21 +0000

    [Feat] load fewshot examples

--- inpars.py
+++ inpars.py
@@ -1,5 +1,27 @@
+import os
+import random
+import pandas as pd
+import urllib.error
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+def load_examples(corpus, n_fewshot_examples):
+    try:
+        df = pd.read_json(
+            f'https://huggingface.co/datasets/inpars/fewshot-examples/resolve/main/data/{corpus}.json',
+            lines=True
+        )
+
+        df = df[['query_id', 'doc_id', 'query', 'document']].values.tolist()
+        random_examples = random.sample(df, n_fewshot_examples)
+        with open('query_ids_to_remove_from_eval.tsv', 'w') as fout:
+            for item in random_examples:
+                fout.write(f'{item[0]}\t{item[2]}\n')
+        return random_examples
+    
+    except urllib.error.HTTPError:
+        return []
 
 class InPars:
     def __init__(
@@ -13,12 +35,12 @@ class InPars:
             max_query_length=None,
             max_prompt_length=None,
             max_new_tokens=64,
-            fp16=False,
-            int8=False,
+            fp16=False, # Use float16 precision to speed up inference
+            int8=False, # Use int8 precision to speed up inference
             device =None,
             tf=False,
-            torch_compile=False,
-            verbose=False
+            torch_compile=False, # Compile the model with torch.jit.script to speed up inference
+            verbose=False # Print out the model input and output
     ):
         self.corpus = corpus
         self.max_doc_length = max_doc_length
@@ -31,4 +53,24 @@ class InPars:
             self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         
         self.tokenizer = AutoTokenizer.from_pretrained(base_model, padding_side="left")
-        
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        model_kwargs = {"revision": revision}
+        if fp16:
+            model_kwargs["torch_dtype"] = torch.float16
+            model_kwargs["low_cpu_mem_usage"] = True
+        if int8:
+            model_kwargs["load_in_8bit"] = True
+            model_kwargs["device_map"] = "auto"
+
+        if fp16 and base_model == "EleutherAI/gpt-j-6B":
+            model_kwargs["revision"] = "float16"
+
+
+        self.model = AutoModelForCausalLM.from_pretrained(base_model, **model_kwargs)
+        if torch_compile:
+            self.model = torch.compile(self.model)
+        self.model.to(self.device)
+        self.model.eval()
+
+        self.fewshot_examples = load_examples(corpus, self.n_fewshot_examples)
