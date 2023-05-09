aad3ef857197658e92bd061c4581e21e71246b7a
kimbyeolhee <qufgml0216@naver.com>
2023-05-09 03:51:49 +0000

    [Feat] 기존에 데이터 파일이 있으면 해당 파일을 불러오도록 변경

--- dataset.py
+++ dataset.py
@@ -7,27 +7,35 @@ def load_corpus(dataset_name, source='ir_datasets'):
     texts = []
     docs_ids = []
 
-    if source == 'ir_datasets':
-        import ir_datasets
+    if not os.path.exists("data"):
+        os.makedirs("data")
 
-        identifier = f'beir/{dataset_name}'
-        if identifier in ir_datasets.registry._registered:
-            dataset = ir_datasets.load(identifier)
-        else:
-            dataset = ir_datasets.load(dataset_name)
-        
-        for doc in tqdm(dataset.docs_iter(), total=dataset.docs_count(), desc="Loading docs from ir-datasets"):
-            texts.append(
-                ftfy.fix_text(
-                    f"{doc.title} {doc.text}" if "title" in dataset.docs_cls()._fields else doc.text
-                )
-            )
-            docs_ids.append(doc.doc_id)
-    
+    if os.path.isfile(f"data/{dataset_name}.csv"):
+        return pd.read_csv(f"data/{dataset_name}.csv")
     else:
-        raise NotImplementedError(f"Source {source} not implemented")
+        if source == 'ir_datasets':
+            import ir_datasets
 
-    return pd.DataFrame({"doc_id" : docs_ids, "text": texts})
+            identifier = f'beir/{dataset_name}'
+            if identifier in ir_datasets.registry._registered:
+                dataset = ir_datasets.load(identifier)
+            else:
+                dataset = ir_datasets.load(dataset_name)
+            
+            for doc in tqdm(dataset.docs_iter(), total=dataset.docs_count(), desc="Loading docs from ir-datasets"):
+                texts.append(
+                    ftfy.fix_text(
+                        f"{doc.title} {doc.text}" if "title" in dataset.docs_cls()._fields else doc.text
+                    )
+                )
+                docs_ids.append(doc.doc_id)
+        
+        else:
+            raise NotImplementedError(f"Source {source} not implemented")
+        df = pd.DataFrame({"doc_id" : docs_ids, "text": texts})
+        df.to_csv(f"data/{dataset_name}.csv", index=False)
+
+        return df
 
 if __name__ == "__main__":
     import argparse
@@ -36,10 +44,6 @@ if __name__ == "__main__":
     parser.add_argument("--source", type=str, default="ir_datasets")
     parser.add_argument("--output", type=str, default="data/msmarco-passage.csv")
     args = parser.parse_args()
-
-    # Create data folder if not exists
-    if not os.path.exists("data"):
-        os.makedirs("data")
 
     df = load_corpus(args.dataset, args.source)
     df.to_csv(args.output, index=False)
